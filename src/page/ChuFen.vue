2d3e7358af736febddae7a5a7415f354432bf63e
xiaoyang <fous9369@gmail.com>
2025-11-15 22:03:14 +0800

    标记表格头和表格体

--- src/page/ChuFen.vue
+++ src/page/ChuFen.vue
@@ -1,9 +1,9 @@
 <script setup>
 import axios from 'axios';
 import { ref } from 'vue';
-const ChuFen =ref([])
+const ChuFen = ref([])
 
-axios.get('http://114.55.2.211:9010/student/info_chufen_search').then(res =>{
+axios.get('http://114.55.2.211:9010/student/info_chufen_search').then(res => {
     const chufen = res.data.data.rows
     ChuFen.value = chufen
 })
@@ -17,22 +17,30 @@ axios.get('http://114.55.2.211:9010/student/info_chufen_search').then(res =>{
         <div class="chufen">
             <h2>受处分学生信息</h2>
             <table>
-                <tr>
-                    <th>学号</th>
-                    <th>姓名</th>
-                    <th>年龄</th>
-                    <th>性别</th>
-                    <th>年级</th>
-                    <th>处分原因</th>
-                </tr>
-                <tr v-for="i in ChuFen" :key="i.id">
-                    <td>{{ i.id }}</td>
-                    <td>{{ i.name }}</td>
-                    <td>{{ i.age }}</td>
-                    <td>{{ i.gender }}</td>
-                    <td>{{ i.grade }}</td>
-                    <td>{{ i.punishment_reason  || '空' }}</td>
-                </tr>
+                <thead>
+
+
+                    <tr>
+                        <th>学号</th>
+                        <th>姓名</th>
+                        <th>年龄</th>
+                        <th>性别</th>
+                        <th>年级</th>
+                        <th>处分原因</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+
+                    <tr v-for="i in ChuFen" :key="i.id">
+                        <td>{{ i.id }}</td>
+                        <td>{{ i.name }}</td>
+                        <td>{{ i.age }}</td>
+                        <td>{{ i.gender }}</td>
+                        <td>{{ i.grade }}</td>
+                        <td>{{ i.punishment_reason || '空' }}</td>
+                    </tr>
+                </tbody>
             </table>
         </div>
         <div class="fenye">
@@ -44,49 +52,57 @@ axios.get('http://114.55.2.211:9010/student/info_chufen_search').then(res =>{
 
 </template>
 <style>
-    input{
-        width: 240px;
-        height: 32px;
-    }
-    .chaxun{
-        width: 58px;
-        height: 30px;
-        border: 1px dashed slategray;
-    }
-    .quanbu{
-        display: flex;
-        flex-direction: column;
-        
-    }
-    .sousuo{
-        margin-top: 15px;
-        margin-left: 20px;
-        display: flex;
-        gap: 8px;
-        align-items: center;
-        
-    }
-    .chufen{
-        text-align: center;
-    }
-    th{
-        background-color: #f3f4f6;
-    }
-    table{
-        width: 100%;
-    }
-    .fenye{
-        display: flex;
-        text-align: center;
-        justify-content: end;
-        margin-right: 50px;
-        margin-top: 20px;
-    }
-    .zuo1{
-        width: 24px;
-        height: 24px;
-        border: 1px solid gray;
-        text-align: center;
-        color: #c5c5c5;
-    }
+input {
+    width: 240px;
+    height: 32px;
+}
+
+.chaxun {
+    width: 58px;
+    height: 30px;
+    border: 1px dashed slategray;
+}
+
+.quanbu {
+    display: flex;
+    flex-direction: column;
+
+}
+
+.sousuo {
+    margin-top: 15px;
+    margin-left: 20px;
+    display: flex;
+    gap: 8px;
+    align-items: center;
+
+}
+
+.chufen {
+    text-align: center;
+}
+
+th {
+    background-color: #f3f4f6;
+}
+
+table {
+    width: 100%;
+}
+
+.fenye {
+    display: flex;
+    text-align: center;
+    justify-content: end;
+    margin-right: 50px;
+    margin-top: 20px;
+}
+
+.zuo1 {
+    width: 24px;
+    height: 24px;
+    border: 1px solid gray;
+    text-align: center;
+    color: #c5c5c5;
+}
 </style>
--- src/page/studentinfo.vue
+++ src/page/studentinfo.vue
@@ -4,10 +4,10 @@ import { ref } from 'vue';
 
 const Inof = ref([])
 
-axios.get('http://114.55.2.211:9010/student/info_search_type').then(res=>{
+axios.get('http://114.55.2.211:9010/student/info_search_type').then(res => {
     const info = res.data.data
-    
-     Inof.value = info 
+
+    Inof.value = info
 })
 </script>
 <template>
@@ -30,25 +30,28 @@ axios.get('http://114.55.2.211:9010/student/info_search_type').then(res=>{
             <h2>学生信息</h2>
             <div class="">
                 <table>
-                    <tr class="ming">
-                        <th>学号</th>
-                        <th>姓名</th>
-                        <th>年龄</th>
-                        <th>性别</th>
-                        <th>年级</th>
-                        <th>是否处分</th>
-                        <th>处分原因</th>
-                    </tr>
-
-                    <tr v-for="i in Inof" :key="i.id">
-                        <td>{{ i.id }}</td>
-                        <td>{{ i.name }}</td>
-                        <td>{{ i.age }}</td>
-                        <td>{{ i.gender }}</td>
-                        <td>{{ i.grade }}</td>
-                        <td>{{ i.punishment }}</td>
-                        <td>{{ i.punishment_reason || '空' }}</td>
-                    </tr>
+                    <thead>
+                        <tr class="ming">
+                            <th>学号</th>
+                            <th>姓名</th>
+                            <th>年龄</th>
+                            <th>性别</th>
+                            <th>年级</th>
+                            <th>是否处分</th>
+                            <th>处分原因</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="i in Inof" :key="i.id">
+                            <td>{{ i.id }}</td>
+                            <td>{{ i.name }}</td>
+                            <td>{{ i.age }}</td>
+                            <td>{{ i.gender }}</td>
+                            <td>{{ i.grade }}</td>
+                            <td>{{ i.punishment }}</td>
+                            <td>{{ i.punishment_reason || '空' }}</td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
         </div>
@@ -62,60 +65,70 @@ axios.get('http://114.55.2.211:9010/student/info_search_type').then(res=>{
 
 </template>
 <style scoped>
-    .quanbu{
-        width: 100%;
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-    }
-    .shousuo{
-        display: flex;
-        gap: 8px;
-        align-items: center;
-        margin-left: 30px;
-        margin-top: 10px;
-    }
-    .shuru{
-        width: 248px;
-        height: 38px;
-    }
-    .duoxuan{
-        width: 80px;
-        height: 32px;
-    }
-    .checkbox{
-        width: 16px;
-        height: 16px;
-    }
-    .chaxun{
-        width: 60px;
-        height: 32px;
-    }
-    .xueshengxinxi{
-        display: flex;
-        text-align: center;
-        flex-direction: column;
-        width: 100%;
+.quanbu {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+}
 
-    }
-    th{
-        background-color: #f3f4f6;
-    }
-    table{
-        width: 100%;
-    }
-    .zuo1{
-        width: 24px;
-        height: 24px;
-        background-color: white;
-        border: 1px solid gray;
-        color: #c5c5c5;
-    }
-    .fenye{
-        display: flex;
-        text-align: center;
-        justify-content: end;
-        margin-right: 50px;
-        margin-top: 20px;
-    }
+.shousuo {
+    display: flex;
+    gap: 8px;
+    align-items: center;
+    margin-left: 30px;
+    margin-top: 10px;
+}
+
+.shuru {
+    width: 248px;
+    height: 38px;
+}
+
+.duoxuan {
+    width: 80px;
+    height: 32px;
+}
+
+.checkbox {
+    width: 16px;
+    height: 16px;
+}
+
+.chaxun {
+    width: 60px;
+    height: 32px;
+}
+
+.xueshengxinxi {
+    display: flex;
+    text-align: center;
+    flex-direction: column;
+    width: 100%;
+
+}
+
+th {
+    background-color: #f3f4f6;
+}
+
+table {
+    width: 100%;
+}
+
+.zuo1 {
+    width: 24px;
+    height: 24px;
+    background-color: white;
+    border: 1px solid gray;
+    color: #c5c5c5;
+}
+
+.fenye {
+    display: flex;
+    text-align: center;
+    justify-content: end;
+    margin-right: 50px;
+    margin-top: 20px;
+}
 </style>
